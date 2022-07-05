2f8b0e3b5329a019abd5a9da733ec32a49650e80
claudiomontoya <claudio.montoya@biopc.cl>
2022-07-04 22:29:33 -0400

    Inicio de repositorio

--- apiVs/Program.cs
+++ apiVs/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,32 @@ namespace apiVs
     {
         static void Main(string[] args)
         {
+            Console.Write("Ingrese texto a buscar: ");
+            string texto = Console.ReadLine();
+            respuestaData data = new respuestaData();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://dummyjson.com/products/search?q="+texto);
+                var response = client.GetAsync("").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = response.Content.ReadAsStringAsync().Result;
+                    data = JsonConvert.DeserializeObject<respuestaData>(responseString);
+                }
+                if (data.products.Count > 0)
+                {
+                    foreach (var item in data.products)
+                    {
+                        Console.WriteLine(item.id +" " + item.title + " Marca: " + item.brand + " Precio:" + item.price);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No se encontraron Articulos!!");
+                }
+
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ apiVs/respuestaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiVs
+{
+    public class respuestaData
+    {
+        public List<Product> products { get; set; }
+    }
+}
+
+
+
+
+
